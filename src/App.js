a35cdcf81cb24da8831bb43449b43e207285f30d
nn nn <nikolasavic.mail@gmail.com>
2020-09-18 11:21:54 +0200

    some comments added into App.js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,14 @@ export default App
 // npm run build
 // npm i -g serve
 // serve -s build
+
+//git config --global user.email "username@example.com"
+// git status
+// git add .
+// git commit -m"poruka"
+
+// GitHub
+// Create new repository
+// Bez Initialize README jer vec ima u reactapp
+// git remote add origin https://github.com/pionirtito/myapp.git // -> gde da pushuje
+// git push -u origin master
