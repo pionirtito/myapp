1972d4190069b734ae6dc94012e4bce92770ecbb
nn nn <nikolasavic.mail@gmail.com>
2020-09-18 10:32:38 +0200

    final app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomePage from './HomePage'
+
+function App() {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    )
+}
+
+export default App
+
+// npm run build
+// npm i -g serve
+// serve -s build
